9ed3ded8b17869b6b9c35f7b12031e93c540092e
adrenaline <sylvain.david@fizians.com>
2015-01-13 11:13:12 +0100

    Remove 'rozo export list' command [rozo]. Updated args for 'rozo export get' command [rozo]. Updated man page [rozo].

--- manager/rozofs/cli/export.py
+++ manager/rozofs/cli/export.py
@@ -24,28 +24,6 @@ from rozofs.cli.output import puts
 from collections import OrderedDict
 
 
-def list(platform, args):
-    e_host = platform._active_export_host
-    configurations = platform.get_configurations([e_host], Role.EXPORTD)
-    if configurations[e_host] is None:
-        raise Exception("%s is not reachable" % e_host)
-
-    configuration = configurations[e_host][Role.EXPORTD]
-
-    list_l = {}
-    exports_l = []
-    for eid, econfig in configuration.exports.items():
-        export_l = []
-        export_l.append({'vid':econfig.vid})
-        export_l.append({'root':econfig.root})
-        export_l.append({'md5':econfig.md5})
-        export_l.append({'squota':econfig.squota})
-        export_l.append({'hquota':econfig.hquota})
-
-        exports_l.append({'EXPORT '+ str(eid):export_l})
-    list_l.update({'EXPORTS': exports_l})
-    ordered_puts(list_l)
-
 def create(platform, args):
     platform.create_export(args.vid[0], args.name, args.passwd, args.squota, args.hquota)
 
@@ -65,14 +43,21 @@ def get(platform, args):
     e_host = platform._active_export_host
     configurations = platform.get_configurations([e_host], Role.EXPORTD)
 
-    if configurations[e_host] is None:
-        raise Exception("exportd node is off line.")
-
     configuration = configurations[e_host][Role.EXPORTD]
+
+    # Check exception
+    if isinstance(configuration, Exception):
+        raise type(configuration)(configuration.message)
+
+    eids_l = []
+    if not args.eids:
+        eids_l = configuration.exports.keys()
+    else:
+        eids_l = args.eids
 
     list_l = {}
     exports_l = []
-    for eid in args.eids:
+    for eid in eids_l:
 
         if eid not in configuration.exports:
             raise Exception("Unknown export with eid=%d." % eid)
@@ -90,14 +75,7 @@ def get(platform, args):
     ordered_puts(list_l)
 
 def dispatch(args):
-    only_export_actions = {'list', 'create', 'update', 'get'}
 
-    if (args.action in only_export_actions):
-        p = Platform(args.exportd, Role.EXPORTD)
-    else:
-        p = Platform(args.exportd)
+    p = Platform(args.exportd, Role.EXPORTD)
 
     globals()[args.action.replace('-', '_')](p, args)
-
-
-
