2a80697847e9499e6716478aef31147974a7edb3
rozodopamine <jean-pierre.monchanin@rozofs.org>
2016-05-26 14:39:11 +0200

    In rozo_fid2pathname use the eid inside FID instead of -p parameter The eid is know from the FID, and the eid root path is known from the configuration file. The mandatory --path parameter disappear and is replaced by an optionnal --config to give the export configuration file name in case it is different from the default one. By the way, the output is formated in JASON format.

--- src/exportd/rozo_fid2pathname.c
+++ src/exportd/rozo_fid2pathname.c
@@ -14,9 +14,15 @@
 #include "export.h"
 #include "rozo_inode_lib.h"
 #include "exp_cache.h"
+#include "econfig.h"
 
-
+int rozofs_no_site_file = 0;
 lv2_cache_t cache;
+econfig_t exportd_config;
+/*
+** Get the default config file name
+*/
+char * configFileName = EXPORTD_DEFAULT_CONFIG;
 
 /*
  *_______________________________________________________________________
@@ -103,7 +109,26 @@ char *rozo_get_full_path(void *exportd,void *inode_p,char *buf,int lenmax)
 
     return pbuf;
 }
+/*
+**_______________________________________________________________________
+*/
+/** Find out the export root path from its eid reading the configuration file
+*   
+    @param  eid : eport identifier
+    
+    @retval -the root path or null when no such eid
+*/
+char * get_export_root_path(uint8_t eid) {
+  list_t          * e;
+  export_config_t * econfig;
 
+  list_for_each_forward(e, &exportd_config.exports) {
+
+    econfig = list_entry(e, export_config_t, list);
+    if (econfig->eid == eid) return econfig->root;   
+  }
+  return NULL;
+}
 /*
  *_______________________________________________________________________
  */
@@ -157,6 +182,28 @@ char *rozo_get_relative_path(void *exportd,void *inode_p,char *buf,int lenmax)
    return buf;
 }
 
+#define TRACEOUT(fmt, ...) { \
+  if (fd_out==NULL) printf(fmt, ##__VA_ARGS__);\
+  else              fprintf(fd_out,fmt, ##__VA_ARGS__);\
+}
+
+#define TRACEFIDPATH(path) { \
+     if (!first) {\
+       TRACEOUT(",\n");\
+     } \
+     first = 0;\
+     TRACEOUT("    {\"FID\" : \"@rozofs_uuid@%s\", \"eid\" : %d, \"path\" : \"%s\"}",fid_buf,current_eid,path);\
+}
+#define TRACEFIDERR(fmt, ...) { \
+     if (!first) {\
+       TRACEOUT(",\n");\
+     } \
+     first = 0;\
+     TRACEOUT("    {\"FID\" : \"@rozofs_uuid@%s\", \"eid\" : %d, \"path\" : \"\", \"error\" : \"",fid_buf,current_eid);\
+     TRACEOUT(fmt,##__VA_ARGS__);\
+     TRACEOUT("\" }");\
+}
+          		 
 /*
  *_______________________________________________________________________
  */
@@ -164,21 +211,21 @@ static void usage() {
     printf("\nUsage: rozo_fid2pathname [-r] -p export_root_path -i <input_filename | fid> [-o output_filename] \n\n");
     printf("Options:\n");
     printf("\t-h,--help:     print this message.\n");
-    printf("\t-p,--path:     exportd root pathname \n");
     printf("\t-r,--relative: when asserted the output is generated with @rozofs_uuid@<FID_parent>/<child_name> format  \n");
     printf("\t-i,--input:    fid of the objet or input filename \n");
     printf("\t               that contains a list of fid in the 1b4e28ba-2fa1-11d2-883f-0016d3cca427 format \n");
     printf("\t-o,--output:   output filename for full path translation (optional) \n\n");
+    printf("\t-c,--config:   exportd configuration file name (when different from %s)\n\n",configFileName);
 
 };
 
 
+eid_t     eid_done[EXPGW_EID_MAX_IDX] = {0};
 
 
 int main(int argc, char *argv[]) {
     int c;
     void *rozofs_export_p;
-    char *root_path=NULL;
     char *input_path=NULL;
     char *output_path=NULL;
     fid_t fid;
@@ -186,7 +233,6 @@ int main(int argc, char *argv[]) {
     lv2_entry_t *plv2;
     int res;
     int count;
-    int failures;
     FILE *fd_in = NULL;
     FILE *fd_out= NULL;
     char name[4096];
@@ -194,13 +240,18 @@ int main(int argc, char *argv[]) {
     char fid_buf[64]; 
     export_t *e;    
     int relative= 0;
+    rozofs_inode_t *inode_val_p;
+    char    * root_path;
+    int       more;
+    int       current_eid;
+    int       first=1;
       
     static struct option long_options[] = {
         {"help", no_argument, 0, 'h'},
-        {"path", required_argument, 0, 'p'},
         {"input", required_argument, 0, 'i'},
         {"output", required_argument, 0, 'o'},
         {"relative", no_argument, 0, 'r'},
+        {"config", required_argument, 0, 'c'},	
         {0, 0, 0, 0}
     };
     
@@ -208,7 +259,7 @@ int main(int argc, char *argv[]) {
     while (1) {
 
       int option_index = 0;
-      c = getopt_long(argc, argv, "hlrc:p:i:o:r", long_options, &option_index);
+      c = getopt_long(argc, argv, "hlrc:i:o:c:r", long_options, &option_index);
 
       if (c == -1)
           break;
@@ -222,15 +273,15 @@ int main(int argc, char *argv[]) {
           case 'r':
               relative = 1;
               break;
-          case 'p':
-              root_path = optarg;
-              break;
-          case 'i':
+         case 'i':
               input_path = optarg;
               break;
           case 'o':
               output_path = optarg;
               break;
+          case 'c':
+              configFileName = optarg;
+              break;			  	      
           case '?':
               usage();
               exit(EXIT_SUCCESS);
@@ -241,14 +292,10 @@ int main(int argc, char *argv[]) {
               break;
       }
   }
-  if (root_path == NULL) 
-  {
-       usage();
-       exit(EXIT_FAILURE);  
-  }
+
   if (input_path == NULL) 
   {
-       printf("Neither an valid input file nor valid FID\n");
+       TRACEOUT("Neither an valid input file nor valid FID\n");
        usage();
        exit(EXIT_FAILURE);  
   }
@@ -260,102 +307,193 @@ int main(int argc, char *argv[]) {
     /*
     **  check the presence of the input file
     */    
-    if ((fd_in = fopen(input_path,"r")) < 0)
+    if ((fd_in = fopen(input_path,"r")) == NULL)
     {
-       printf("not a valid file: %s: %s\n",input_path,strerror(errno));
+       TRACEOUT("not a valid file: %s: %s\n",input_path,strerror(errno));
        usage();
        exit(EXIT_FAILURE);      
-    }      
+    }          
   }
   else
   {
     one_fid_only = 1;
+    strcpy(fid_buf,input_path);
   }
   /*
   ** Check for ouput file
   */
   if (output_path != NULL)
   {
-    if ((fd_out = fopen(output_path,"w")) < 0)
+    if ((fd_out = fopen(output_path,"w")) == NULL)
     {
-       printf("not a valid file: %s: %s\n",input_path,strerror(errno));
+       TRACEOUT("fopen(%s) %s\n",input_path,strerror(errno));
        usage();
        exit(EXIT_FAILURE);      
     }       
   }
-
+  
+  /*
+  ** Read configuration file
+  */
+  if (econfig_initialize(&exportd_config) != 0) {
+       TRACEOUT("can't initialize exportd config %s.\n",strerror(errno));
+       exit(EXIT_FAILURE);  
+  }    
+  if (econfig_read(&exportd_config, configFileName) != 0) {
+       TRACEOUT("failed to parse configuration file %s %s.\n",configFileName,strerror(errno));
+       exit(EXIT_FAILURE);  
+  }   
+ 
+  
   /*
   ** init of the RozoFS data structure on export
   ** in order to permit the scanning of the exportd
   */
-  rozofs_export_p = rz_inode_lib_init(root_path);
-  if (rozofs_export_p == NULL)
-  {
-    printf("RozoFS: error while reading %s\n",root_path);
-    exit(EXIT_FAILURE);  
-  }
-  /*
-  ** init of the lv2 cache
-  */
-  lv2_cache_initialize(&cache);
-  failures = 0;
-  count = 0;
-  e =  rozofs_export_p;
-  res = 1;
 
-  if (one_fid_only == 0) res= fscanf(fd_in,"%s",fid_buf);
-  while ((res != EOF) || (one_fid_only))
-  {
-      /*
-      ** Parse the fid
-      */
-     count++;
-     while (1)
-     {
-       if (one_fid_only == 0)
+
+  count = 0;  
+  current_eid = 0;
+  more = 0;
+  rozofs_export_p = NULL;
+  TRACEOUT("{ \"FID list\" : [\n");   
+  while (1) {
+
+     /*
+     ** Get next FID
+     */
+     if (one_fid_only){
+       /*
+       ** Not to reloop
+       */
+       if (more) break;
+       more = 1;
+       res  = 0;
+     }     
+     else {
+       res = fscanf(fd_in,"%s",fid_buf); 
+     }
+  
+     /*
+     ** End of file
+     */
+     if (res == EOF) {
+       /*
+       ** Current eid is done
+       */
+       eid_done[current_eid] = 1;
+       /*
+       ** Free previous export resources
+       */
+       if (rozofs_export_p != NULL) {
+	 free(rozofs_export_p);
+	 lv2_cache_release (&cache); 
+	 rozofs_export_p = NULL;
+       }
+       /*
+       ** Is there more eid to process ? 
+       */
+       if (more==0) break; // No. We are done.
+
+       /*
+       ** Rewind the file to the beginning
+       */
+       fclose(fd_in);
+       if ((fd_in = fopen(input_path,"r")) < 0)
        {
-	 if (rozofs_uuid_parse(fid_buf, fid)<0) 
-	 {
-            if (fd_out==NULL) {
-	      printf("%s : cannot be translated\n",fid_buf);
-	    }
-	    else
-	    {
-	      fprintf(fd_out,"%s : cannot be translated\n",fid_buf);	
-	    }
-	    failures +=1;
-	    break;
-	  }
-	}
-	/*
-	** Get the attributes of the file
-	*/
-	if (!(plv2 = EXPORT_LOOKUP_FID(e->trk_tb_p,&cache, fid))) {
-	  if (fd_out) fprintf(fd_out,"@rozofs_uuid@%s : cannot be translated\n",fid_buf);	
-	  else  printf("@rozofs_uuid@%s : cannot be translated\n",fid_buf);
-	  failures +=1;
-	  break;
-        }
-	/*
-	** Get the full pathname
-	*/
-        if (relative)
-	  pbuf = rozo_get_relative_path(rozofs_export_p,&plv2->attributes,name,4096);
-	else
-          pbuf = rozo_get_full_path(rozofs_export_p,&plv2->attributes,name,4096);
-        if (fd_out==NULL) {
-          printf("%s\n",pbuf);
-	}
-	else
-	{
-	  fprintf(fd_out,"%s\n",pbuf);	
-	}
-	break;
+	  TRACEFIDERR("re fopen(%s) %s",input_path,strerror(errno));
+	  exit(EXIT_FAILURE);      
+       }
+       /*
+       ** Read the first FID
+       */
+       res = fscanf(fd_in,"%s",fid_buf); 
+       if (res < 0) {
+	  TRACEFIDERR("Can not re-read file %s %s\n",input_path,strerror(errno)); 
+	  break;         
+       } 
+       more        = 0;
+       current_eid = 0;         
      }  
-     if (one_fid_only) break;
-     res= fscanf(fd_in,"%s",fid_buf);  
+  
+     /*
+     ** Parse the fid
+     */
+     if (rozofs_uuid_parse(fid_buf, fid)<0) 
+     {
+       TRACEFIDERR("cannot be translated");       
+       continue;
+     }
+
+     /*
+     ** Extract the eid from the FID
+     */       
+     inode_val_p = (rozofs_inode_t *) fid;
+
+     /*
+     ** 1rst FID encountered
+     */
+     if (current_eid == 0) {
+     
+       current_eid = inode_val_p->s.eid;
+       if (eid_done[current_eid] == 1) {
+         /*
+	 ** This eid has already been processed
+	 */
+	 current_eid = 0;
+	 continue;
+       }
+       	 
+       /*
+       ** Find the export root path
+       */
+       root_path = get_export_root_path(current_eid);
+       if (root_path==NULL) {
+         TRACEFIDERR("eid %d is not configured",current_eid);       
+	 current_eid = 0;
+	 continue;	   	
+       }
+
+       rozofs_export_p = rz_inode_lib_init(root_path);
+       e =  rozofs_export_p;  
+       if (rozofs_export_p == NULL) {
+         TRACEFIDERR("error while reading eid %d path %s",current_eid,root_path);       	 
+	 exit(EXIT_FAILURE);  
+       }
+       /*
+       ** init of the lv2 cache
+       */
+       lv2_cache_initialize(&cache);	 
+     }
+
+     /*
+     ** This is not the current eid
+     ** We will process the FID later if not already done
+     */
+     if (inode_val_p->s.eid != current_eid) {
+       if (eid_done[inode_val_p->s.eid] == 0) {
+         more = 1;
+       }	 
+       continue;
+     }	  
+       
+     /*
+     ** Get the attributes of the file
+     */
+     if (!(plv2 = EXPORT_LOOKUP_FID(e->trk_tb_p,&cache, fid))) {
+       TRACEFIDERR("No such file or directory");
+       continue;
+     }
+     /*
+     ** Get the full pathname
+     */
+     count++;
+     if (relative)
+       pbuf = rozo_get_relative_path(rozofs_export_p,&plv2->attributes,name,4096);
+     else
+       pbuf = rozo_get_full_path(rozofs_export_p,&plv2->attributes,name,4096);
+     TRACEFIDPATH(pbuf);  	
   }
-  printf("Number of scanned/failed FID: %d/%d\n",count,failures);
+  TRACEOUT("\n  ]\n}\n");   
   if (fd_out) fclose(fd_out);
   if (fd_in) fclose(fd_in);
   
